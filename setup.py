33af435ffd724ae896bc935839e3ecfe6cb39f0a
Hiroki KIYOHARA <hirokiky@gmail.com>
2020-03-02 22:47:38 +0900

    For publish as package.

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
+import os
+
 from setuptools import find_packages, setup
 
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
 
 setup(
     name="ruin",
     version="0.0.1",
-    install_requires=[
-        "jinja2==2.11.1",
-        "libsass==0.19.4",
-    ],
+    install_requires=["jinja2==2.11.1", "libsass==0.19.4",],
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'ruin = ruin.main:main',
-        ],
-    },
+    license="MIT",
+    author="Hiroki Kiyohara",
+    author_email="hirokiky@gmail.com",
+    description="Static page generator",
+    long_description=README,
+    entry_points={"console_scripts": ["ruin = ruin.main:main",],},
 )
